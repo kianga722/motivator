23e6f693ccfff53468f90050a00b17689af7a7ad
kianga722 <kianga722@gmail.com>
2019-04-22 23:36:33 -0400

    Edit production logic

--- server.js
+++ server.js
@@ -39,8 +39,12 @@ app.get('/api/', (req, res) => {
 
 
 if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
 
 // Port
