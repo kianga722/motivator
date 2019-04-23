0e507e66cbb3bce513b6b70c14e7760cb1cde9a6
kianga722 <kianga722@gmail.com>
2019-04-23 00:17:12 -0400

    Trying to fix api error

--- server.js
+++ server.js
@@ -39,12 +39,8 @@ app.get('/api/', (req, res) => {
 
 
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
 }
 
 // Port
